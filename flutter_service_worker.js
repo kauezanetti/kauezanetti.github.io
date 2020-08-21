f33617676815f7003811a355ba08d6239e09d64a
Kaue Zanetti <kauezanetti@hotmail.com>
2020-08-21 03:13:19 -0300

    alterando o html para ver a alteracao no pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,17 +3,20 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "64b5fc86e1ccc5cd2c1966cb79dfe796",
-"/": "64b5fc86e1ccc5cd2c1966cb79dfe796",
-"main.dart.js": "67302052acca00dfec21b318e1231cc1",
+  "index.html": "45881c11a5d69aa9662f4b8bf30ea6cc",
+"/": "45881c11a5d69aa9662f4b8bf30ea6cc",
+"main.dart.js": "6160a249a28680eea4a4ff76efdc060a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"manifest.json": "625bebb2e92b8b6e7a50dbff99959634",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"manifest.json": "ae6984dd84f180264462c4abbbfbab60",
+"assets/AssetManifest.json": "801125fca546f506babac9af4ec061ba",
 "assets/NOTICES": "108d0a1640e6b622b04c79e5a1de9f51",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
 
